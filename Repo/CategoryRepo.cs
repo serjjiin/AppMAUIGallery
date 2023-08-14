cec91d5500757b2403201003d60e2a5b1101ccbc
Sérgio Lima <slgrippa2@gmail.com>
2023-08-14 17:18:28 -0300

    Adicionar arquivos de projeto.

---
+++ Repo/CategoryRepo.cs
@@ -0,0 +1,28 @@
+﻿using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Layouts;
+
+namespace AppMAUIGallery.Repo
+{
+    internal class CategoryRepo
+    {
+        public CategoryRepo() { }
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category
+                {
+                    Name = "Layout",
+                    Components = new List<Component>
+                {
+                    new Component
+                    {
+                    Title = "StackLayout", Description = "Loren blabalbla.", Page = new StackLayoutPage()
+                    }
+                }
+                }
+            };
+            return categories; 
+        }
+    }
+}
